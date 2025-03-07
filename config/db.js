0e5ff166acde39127af1dcd8d39d991bdd02e66b
gdefrr00 <gdefrr00@estudiantes.unileon.es>
2025-03-07 10:26:39 +0100

    añadida librería dotenv y nuevo acceso a las credenciales

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
 //const MONGO_URI = 'mongodb://localhost:27017/sensores';
-const MONGO_URI = 'mongodb://usuario:contraseña@localhost:27017/sensores?authSource=admin'
+const MONGO_URI = process.env.MONGO_CREDENTIALS || 'mongodb://localhost:27017/sensores'; // Hacemos que la url que tenemos la coja como variable de entorno
+                                                                                         // y si no la encuentra, coja la que le ponemos deslpués del ||.
+                                                                                         // De esta manera, es mucho más portable y no compartimos las credenciales.
 
 const connectDB = async () => {
     try {
